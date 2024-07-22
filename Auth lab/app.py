a92ee4961aa7c1a34ed1a0ba2d12b504ab56855c
talinesh24-meet <st.taline.shweiki@rosaryshs-j.com>
2024-07-22 11:26:11 +0300

    auth lab

--- Auth lab/app.py
+++ Auth lab/app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,url_for,redirect,request
 from flask import session
 import pyrebase
 
-app = Flask(__name__,template_folder="template",static_folder='static')
+app = Flask(__name__,template_folder="templates",static_folder='static')
 
 Config = {
   "apiKey": "AIzaSyDGarSTibNmc8OmVEameK9AjpbhY8p8tSs",
@@ -11,7 +11,7 @@ Config = {
   "storageBucket": "auth-lab-9db17.appspot.com",
   "messagingSenderId": "58834721290",
   "appId": "1:58834721290:web:562e60a8763676dc6e1df3",
-  "measurementId": "G-MPFP3BS75R"
+  "measurementId": "G-MPFP3BS75R",
   "databaseURL": "https://auth-lab-9db17-default-rtdb.firebaseio.com"
 }
 
@@ -22,16 +22,19 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 @app.route('/',methods=['GET', 'POST'])
 def signup():
   
-   if request.method == 'POST':
+  if request.method == 'POST':
        email = request.form['email']
        password = request.form['password']
        session['quotes'] = []
        try:
-          login_session['user'] = auth.create_user_with_email_and_password(email, password)
-          return redirect(url_for('/home'))
+          session['user'] = auth.create_user_with_email_and_password(email, password)
+          return redirect('/home')
        except:
            error = "Authentication failed"
-   return render_template("signup.html")
+           print(error)
+           return render_template("error.html")
+  else:
+      return render_template("signup.html")
 
 
 @app.route('/signin',methods=['GET', 'POST'])
@@ -42,9 +45,12 @@ def signin():
        session['quotes'] = []
        try:
           login_session['user'] = auth.sign_in_with_email_and_password(email, password)
-          return redirect(url_for('/home'))
+          return redirect(url_for('home'))
        except:
            error = "Authentication failed"
+           print(error)
+           redirect('/error')
+
   return render_template("signin.html")
 
 
@@ -52,27 +58,38 @@ def signin():
 def signout():
   auth.signOut()
   login_session['user'] = None
-  return redirect(url_for('/signin'))
+  return redirect(url_for('signin'))
 
 
 
 @app.route('/home',methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        quote = request.form['quotes']
-        session['quotes'].append(quote)
-    return redirect(url_for('/thanks'))
+      quote = request.form['quote']
+
+      session['quotes'].append(quote)
+
+      session.modified = True
+
+      return redirect(url_for('thanks'))
+    else:
+      return render_template("home.html")
+
    
 
 
 @app.route('/display',methods=['GET', 'POST'])
-def display(quotes):
-  quotes=login.session['quotes']
-  return render_template("display.html",quotes=quotes)
+def display():
+  return render_template("display.html",quotes = session['quotes'])
 
 @app.route('/thanks',methods=['GET', 'POST'])
 def thanks():
   return render_template('thanks.html')
+
+@app.route('/error',methods=['GET', 'POST'])
+def error():
+  return render_template('error.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
